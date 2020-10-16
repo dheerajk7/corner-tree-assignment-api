160d7f8c2399698b69d16975a744c08b70ee31b0
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-10-16 19:54:49 +0530

    update mongoose config-2

--- config/environment.js
+++ config/environment.js
@@ -5,12 +5,9 @@ const development = {
 };
 
 const production = {
-  name: process.env.env_name,
+  name: "production",
   db: "corner-tree-assignment",
   db_path: `mongodb+srv://jevik-zayka-user:${process.env.password}@cluster0.cmbhb.mongodb.net/corner-tree-assignment>?retryWrites=true&w=majority`,
 };
 
-module.exports =
-  eval(process.env.env_name) == undefined
-    ? development
-    : eval(process.env.env_name);
+module.exports = production;
