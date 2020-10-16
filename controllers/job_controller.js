2ae2ebc9523f6555af48f61162f1b47254503c72
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-10-16 19:21:38 +0530

    add new job post

---
+++ controllers/job_controller.js
@@ -0,0 +1,43 @@
+const Job = require("../models/job");
+
+module.exports.createJob = async function (request, response) {
+  try {
+    let job = await Job.create({
+      role: request.body.role,
+      function: request.body.function_value,
+      sub_function: request.body.sub_function,
+      company: request.body.company,
+      location: request.body.location,
+      pincode: request.body.pincode,
+      compensation: request.body.compensation,
+      job_description: request.body.job_description,
+      core_skills: request.body.core_skills_list,
+      soft_skills: request.body.soft_skills_list,
+    });
+    console.log(job);
+    if (job) {
+      return response
+        .status(200)
+        .json({ success: "true", message: "Job Posted Successfully" });
+    } else {
+      return response
+        .status(400)
+        .json({ success: false, message: "Error in posting job" });
+    }
+  } catch (err) {
+    return response.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+module.exports.search = async function (request, response) {
+  try {
+  } catch (err) {
+    return response.status(200).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+//accessing home controller
+var jobController = require("../controllers/job_controller");
+
+//routes
+router.post("/api/v1/create-job", jobController.createJob);
+router.post("/api/v1/search/:key", jobController.search);
+
+module.exports = router;
